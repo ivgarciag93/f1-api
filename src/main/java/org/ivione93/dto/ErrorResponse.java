c0804a6ab7da103f0cc7361208695ebcf569c5b7
ivgarciag93 <ivgarciag93@gmail.com>
2026-07-04 17:51:31 +0200

    feat: first commit

---
+++ src/main/java/org/ivione93/dto/ErrorResponse.java
@@ -0,0 +1,17 @@
+package org.ivione93.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+
+  public String error;
+  public int code;
+
+  public ErrorResponse(String error, int code) {
+    this.error = error;
+    this.code = code;
+  }
+}
